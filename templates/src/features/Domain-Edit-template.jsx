d75bd148372c97e74fbe388497b26232c2ef119c
Lawrence Rama <oleng_rama@hotmail.com>
2024-12-28 20:44:55 -0500

    fixed up the layout of Edit screen

--- templates/src/features/Domain-Edit-template.jsx
+++ templates/src/features/Domain-Edit-template.jsx
@@ -1,6 +1,6 @@
 
 import React from "react";
-import { TextField, Button, Grid } from "@mui/material";
+import { TextField, Button, Grid, Container, Box } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import { edited, save{{domainName}} } from "./{{domainCamelCase}}Slice";
 
@@ -20,27 +20,31 @@ function {{domainName}}Edit() {
   }
 
   return (
-    <Grid container spacing={2}>
-      <form>
-        {{#each attributes}}
-        <Grid item xs={12}>
-          <TextField
-            id="{{this.attributeName}}"
-            name="{{this.attributeName}}"
-            label="{{this.attributeName}}"
-            value={selected{{../domainName}}.{{this.attributeName}} }
-            onChange={onEdit{{../domainName}} }
-            fullWidth
-          />
-        </Grid>
-        {{/each}}
-        <Grid item xs={12}>
-          <Button variant="contained" color="primary" onClick={buttonEventHandler}>
-            Save {{domainName}}
-          </Button>
-        </Grid>
-      </form>
-    </Grid>
+    <Container maxWidth="sm">
+      <Box mt={4}>        
+          <form>
+            <Grid container spacing={2}>
+            {{#each attributes}}
+            <Grid item xs={12}>
+              <TextField
+                id="{{this.attributeName}}"
+                name="{{this.attributeName}}"
+                label="{{this.attributeName}}"
+                value={selected{{../domainName}}.{{this.attributeName}} }
+                onChange={onEdit{{../domainName}} }
+                fullWidth
+              />
+            </Grid>
+            {{/each}}
+            <Grid item xs={12}>
+              <Button variant="contained" color="primary" onClick={buttonEventHandler}>
+                Save {{domainName}}
+              </Button>
+            </Grid>
+            </Grid>
+          </form>        
+      </Box>
+    </Container>
 
   );
 }
